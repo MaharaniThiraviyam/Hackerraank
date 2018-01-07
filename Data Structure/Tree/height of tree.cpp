3f0b029772f52a7af8f629dbc698a141e846b4d7
T.MAHARANI <maharani1118@gmail.com>
2018-01-07 20:35:47 +0530

    Create height of tree.cpp

---
+++ Data Structure/Tree/height of tree.cpp
@@ -0,0 +1,19 @@
+}; //End of Solution
+
+int main() {
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.height(root);
+    cout << height;
+
+    return 0;
+}
