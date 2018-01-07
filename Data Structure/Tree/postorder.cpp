1bfd646a3ab1d2baa19198194283c521d063a39d
T.MAHARANI <maharani1118@gmail.com>
2018-01-07 20:11:49 +0530

    Create postorder.cpp

---
+++ Data Structure/Tree/postorder.cpp
@@ -0,0 +1,23 @@
+/* you only have to complete the function given below.  
+Node is defined as  
+
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+};
+
+*/
+
+
+void postOrder(node *root) {
+     if (root!= NULL)
+    {
+        postOrder(root->left);
+        postOrder(root->right);
+        cout<<root->data<<" ";
+       
+     }
+
+}
