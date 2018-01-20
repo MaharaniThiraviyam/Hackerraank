3cfab88e3233ec6535934ef7df219b5a83140540
T.MAHARANI <maharani1118@gmail.com>
2018-01-20 17:43:59 +0530

    Create pangrams.cpp

---
+++ Algorithms/String/pangrams.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    char s[1000];
+    int i,used[26]={0},total=0;
+    gets(s);
+
+    for(i=0;s[i]!='\0';i++)
+    {
+        if('a'<=s[i] && s[i]<='z')
+        {
+            total+=!used[s[i]-'a'];
+            used[s[i]-'a']=1;
+        }
+        else if('A'<=s[i] && s[i]<='Z')
+        {
+            total+=!used[s[i]-'A'];
+            used[s[i]-'A']=1;
+        }
+    }
+
+    if(total==26)
+        cout<<"pangram";
+    else
+          cout<<"not pangram";
+
+    return 0;
+}
+
