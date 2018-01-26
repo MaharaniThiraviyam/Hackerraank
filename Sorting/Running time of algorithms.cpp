1d0a9262f0a614822a9150d89bfd16bcd9f7d641
T.MAHARANI <maharani1118@gmail.com>
2018-01-26 17:18:12 +0530

    Create Running time of algorithms.cpp

---
+++ Sorting/Running time of algorithms.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+      int a[1000],i,j,key,n,z,count=0;
+  cin>>n;
+  for(i=1;i<=n;i++)
+   cin>>a[i];
+  for(i=2;i<=n;i++){
+   for(key=a[i],j=i-1;j>0 && a[j]>key;j--){
+
+    a[j+1]=a[j];
+     count++;
+  }
+    a[j+1]=key;
+  }
+    cout<<count;
+
+    return 0;
+    
+}
