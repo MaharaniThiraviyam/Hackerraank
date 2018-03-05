478804fbc54e189920a777a121bbb5e1570b9b7e
T.MAHARANI <maharani1118@gmail.com>
2018-03-05 20:16:29 +0530

    Create currency formatter.java

---
+++ JAVA/currency formatter.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+      
+        
+        Locale INDIA = new Locale("en","IN");
+        
+        NumberFormat us    = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india  = NumberFormat.getCurrencyInstance(INDIA);
+        NumberFormat china  = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        // Write your code here.
+        
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
